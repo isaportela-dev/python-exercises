7e728dfbd1f60c753ac1c460643f6796c5159921
Isabella Portela <isaportela6@gmail.com>
2026-03-27 21:47:22 -0300

    Create sum_function.py

---
+++ functions/sum_function.py
@@ -0,0 +1,11 @@
+# This function returns the sum of two numbers
+
+def sum_numbers(a, b):
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = sum_numbers(num1, num2)
+
+print("Sum:", result)
