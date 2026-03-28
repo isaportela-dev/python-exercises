f9d39f5c43cdcdbdfa475662bb0b06acec0bdc21
Isabella Portela <isaportela6@gmail.com>
2026-03-27 21:47:05 -0300

    Create even_or_odd_function.py

---
+++ functions/even_or_odd_function.py
@@ -0,0 +1,12 @@
+# This function checks if a number is even or odd
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+num = int(input("Enter a number: "))
+result = check_even_odd(num)
+
+print(result)
